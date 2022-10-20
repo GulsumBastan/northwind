679444685cd8490df99dd49441f9cbd7d34cd4b6
Gülsüm Baştan <102551681+GulsumBastan@users.noreply.github.com>
2022-10-20 16:34:18 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
-import { TodoComponent } from './components/todo/todo.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,11 +14,12 @@ import { TodoComponent } from './components/todo/todo.component';
     ProductComponent,
     CategoryComponent,
     NaviComponent,
-    TodoComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
-//import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
 
 @Component({
   selector: 'app-product',
